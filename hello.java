5ab012914eb7ce0570c37a9a7fd40061404e141b
Sudeepbr <sudeep9242123@gmail.com>
2026-04-08 14:52:38 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-// Simple Java program to print "Hello"
 public class Hello {
     public static void main(String[] args) {
-        // Print message to the console
         System.out.println("Hello");
     }
 }
