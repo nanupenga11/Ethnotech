784aca459a2f88f2d0a8656db68ca0863eab2e48
Sudeepbr <sudeep9242123@gmail.com>
2026-04-08 14:48:05 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,7 @@
+// Simple Java program to print "Hello"
+public class Hello {
+    public static void main(String[] args) {
+        // Print message to the console
+        System.out.println("Hello");
+    }
+}
